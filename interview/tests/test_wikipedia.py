c9710cba3bc998a5cb6b924097212fa389049052
Ross Patterson <me@rpatterson.net>
2018-03-30 12:21:47 -0700

    Add input format parsing class

--- interview/tests/test_wikipedia.py
+++ interview/tests/test_wikipedia.py
@@ -5,6 +5,8 @@ Test the wikipedia question answering implementation.
 import os
 import unittest
 
+import textblob
+
 from interview import wikipedia
 
 
@@ -15,19 +17,71 @@ class TestWikipedia(unittest.TestCase):
 
     SAMPLE = 'zebras'
 
+    def setUp(self):
+        """
+        Load the samples for testing against.
+        """
+        super(TestWikipedia, self).setUp()
+
+        with open(os.path.join(
+                os.path.dirname(__file__), self.SAMPLE + '.input.txt'
+        )) as input_file:
+            self.input_text = input_file.read()
+        with open(os.path.join(
+                os.path.dirname(__file__), self.SAMPLE + '.output.txt'
+        )) as output_file:
+            self.expected_output_text = output_file.read()
+
+    def test_input_format(self):
+        """
+        Test parsing the input format.
+        """
+        input_text = wikipedia.InputText(self.input_text)
+
+        self.assertIn(
+            'paragraph', dir(input_text), 'Missing parsed paragraph')
+        self.assertIsInstance(
+            input_text.paragraph, textblob.TextBlob,
+            'Wrong parsed paragraph type')
+        self.assertTrue(
+            input_text.paragraph,
+            'Missing parsed paragraph content')
+
+        self.assertIn(
+            'questions', dir(input_text), 'Missing parsed questions')
+        self.assertIsInstance(
+            input_text.questions, list,
+            'Wrong parsed questions type')
+        self.assertTrue(
+            input_text.questions,
+            'Missing parsed questions content')
+        self.assertIsInstance(
+            input_text.questions[0], textblob.TextBlob,
+            'Wrong parsed question type')
+        self.assertTrue(
+            input_text.questions[0],
+            'Missing parsed question content')
+
+        self.assertIn(
+            'answers', dir(input_text), 'Missing parsed answers')
+        self.assertIsInstance(
+            input_text.answers, list,
+            'Wrong parsed answers type')
+        self.assertTrue(
+            input_text.answers,
+            'Missing parsed answers content')
+        self.assertIsInstance(
+            input_text.answers[0], textblob.TextBlob,
+            'Wrong parsed answer type')
+        self.assertTrue(
+            input_text.answers[0],
+            'Missing parsed answer content')
+
     def test_sample(self):
         """
         Test the sample input against the sample output.
         """
-        with open(os.path.join(
-                os.path.dirname(__file__), self.SAMPLE + '.input.txt'
-        )) as input_file:
-            input_text = input_file.read()
-        with open(os.path.join(
-                os.path.dirname(__file__), self.SAMPLE + '.output.txt'
-        )) as output_file:
-            expected_output_text = output_file.read()
-        output_text = wikipedia.match_question_answers(input_text)
+        output_text = wikipedia.match_question_answers(self.input_text)
         self.assertEqual(
-            output_text, expected_output_text,
+            output_text, self.expected_output_text,
             'Did not match expected output')
--- interview/wikipedia.py
+++ interview/wikipedia.py
@@ -2,13 +2,43 @@
 Utilities for processing Wikipedia text.
 """
 
+import textblob
+
+
+class InputText(str):
+    """
+    Parse the input format into a paragraph, questions and answers.
+    """
+
+    def __init__(self, input_text=''):
+        """
+        Parse the input format into a paragraph, questions and answers.
+        """
+        super(InputText, self).__init__()
+
+        lines = input_text.strip().split('\n')
+        # "The first line contains a short paragraph of text from
+        # Wikipedia."
+        self.paragraph = textblob.TextBlob(lines[0])
+        # "Lines 2 to 6 contain a total of 5 questions."
+        # NOTE Because we can assume the last line contains the
+        # answers, we can support an arbitrary number of questions
+        self.questions = [
+            textblob.TextBlob(question) for question in lines[1:-1]]
+        # "Line 7 contains all the five answers, which are jumbled up."
+        self.answers = [
+            textblob.TextBlob(answer.strip())
+            for answer in lines[-1].split(';')]
+        # TODO the above could be changed to be processed lazily on
+        # first access to avoid the TextBlob processing time if ever
+        # there was significant use where only some of the attributes
+        # are referenced.  Before doing so, confirm that TextBlob
+        # doesn't already to laze processing
+
 
 def match_question_answers(input_text):
     """
     Match answers to their questions based on the input paragraph.
     """
-    lines = input_text.strip().split('\n')
-    paragraph = lines[0]
-    questions = lines[1:-1]
-    answers = [answer.strip() for answer in lines[-1].split(';')]
-    return '\n'.join(answers)
+    parsed = InputText(input_text)
+    return '\n'.join(str(answer) for answer in parsed.answers) + '\n'
